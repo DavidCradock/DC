4d13d589dbb6871b04679a0ab128df7cb6f4db3d
David Cradock <djpcradock@gmail.com>
2023-10-24 16:01:03 +0100

    Added beginnings of a manager for fragment programs.

--- DavesCodeLib/Renderer/fragmentProgramManager.h
+++ DavesCodeLib/Renderer/fragmentProgramManager.h
@@ -1,15 +1,23 @@
 #pragma once
 #include "fragmentProgram.h"
+#include <map>
 
 namespace DC
 {
 	// Manager class to handle fragment programs.
 	// The Renderer class has a public object of this class ready to use.
+	// Objects are reference counted.
 	class FragmentProgramManager
 	{
 	public:
+		// Constructor
+		FragmentProgramManager();
+		
+		// Destructor
+		~FragmentProgramManager();
+
 		// Adds a new uniquely named object.
-		// If the named object already exists, it's reference count is increased.
+		// If the named object already exists, it's reference count is increased and the existing object is returned.
 		FragmentProgram* add(const String& objectName);
 
 		// Returns whether the named object exists or not
@@ -30,16 +38,24 @@ namespace DC
 		// Use getNumber() to determine valid index range.
 		String getName(size_t index);
 
-		// Returns the number of objects.
+		// Returns the number of objects currently added with add().
 		size_t getNumber(void);
 
 		// Attempts to remove the named object.
 		// If the named object does not exist, an exception occurs.
-		// If the named object exists and has been added multiple times, it's reference count is decreased and the object remains.
-		void remove(const String& objectName);
+		// If the named object exists and has been added multiple times, it's reference count is decreased and the object remains and it's pointer is returned, else 0.
+		FragmentProgram* remove(const String& objectName);
 
 		// Removes all objects.
-		// forceRemoval, if true will remove objects regardless of their reference count, else only objects with a reference count of 0 will be removed.
+		// forceRemoval, if true will remove objects regardless of their reference count, else only objects with a reference count of 1 will be removed.
 		void removeAll(bool forceRemoval = false);
+
+	private:
+		struct Object
+		{
+			FragmentProgram* object;
+			unsigned int referenceCount;
+		};
+		std::map<String, Object*> objects;
 	};
 }
